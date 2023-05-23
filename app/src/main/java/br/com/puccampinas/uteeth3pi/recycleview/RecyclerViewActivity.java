2f75d16bdd3d7bbcd32837eff8b7b7390d06c3cb
guiga <sousadominguesgui@hotmail.com>
2023-05-22 22:51:07 -0300

    RecyclerView implementation

--- app/src/main/java/br/com/puccampinas/uteeth3pi/recycleview/RecyclerViewActivity.java
+++ app/src/main/java/br/com/puccampinas/uteeth3pi/recycleview/RecyclerViewActivity.java
@@ -1,13 +1,21 @@
 package br.com.puccampinas.uteeth3pi.recycleview;
 
+import android.app.ProgressDialog;
 import android.media.metrics.Event;
 import android.os.Bundle;
+import android.util.Log;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.ktx.Firebase;
 
 import java.util.ArrayList;
@@ -16,16 +24,24 @@ import br.com.puccampinas.uteeth3pi.R;
 
 public class RecyclerViewActivity extends AppCompatActivity {
 
+
     RecyclerView recyclerView;
     ArrayList<User> userArrayList;
     MyAdapter myAdapter;
     FirebaseFirestore db;
+
+    ProgressDialog progressDialog;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view);
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setCancelable(false);
+        progressDialog.setMessage("Fetching Data...");
+        progressDialog.show();
 
         recyclerView=findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
@@ -35,7 +51,39 @@ public class RecyclerViewActivity extends AppCompatActivity {
         userArrayList=new ArrayList<User>();
         myAdapter = new MyAdapter(RecyclerViewActivity.this,userArrayList);
 
+        recyclerView.setAdapter(myAdapter);
+
+        EventChangeListener();
 
 
+
+
+    }
+
+    private void EventChangeListener() {
+        db.collection("RecyclerViewTeste").orderBy("name", Query.Direction.ASCENDING)
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                        if (error != null){
+
+                            if (progressDialog.isShowing())
+                                progressDialog.dismiss();
+                            Log.e("Firestore error",error.getMessage());
+                            return;
+                        }
+
+                        for (DocumentChange dc : value.getDocumentChanges()){
+                            if (dc.getType() == DocumentChange.Type.ADDED){
+                                userArrayList.add(dc.getDocument().toObject(User.class));
+
+                            }
+                            myAdapter.notifyDataSetChanged();
+                            if (progressDialog.isShowing())
+                                progressDialog.dismiss();
+                        }
+
+                    }
+                });
     }
 }
