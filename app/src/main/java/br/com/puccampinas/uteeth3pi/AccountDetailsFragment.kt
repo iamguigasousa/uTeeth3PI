25367fa7f1b1ba88641bf58d97b16d102feadc3a
Pedro Lyra <127896730+pedrolyraa@users.noreply.github.com>
2023-05-24 22:13:44 -0300

    Update Dados - AccountDetailsFragment Funcionando

--- app/src/main/java/br/com/puccampinas/uteeth3pi/AccountDetailsFragment.kt
+++ app/src/main/java/br/com/puccampinas/uteeth3pi/AccountDetailsFragment.kt
@@ -10,14 +10,18 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import br.com.puccampinas.uteeth3pi.databinding.FragmentAccountDetailsBinding
 import br.com.puccampinas.uteeth3pi.databinding.FragmentHomeMenuBinding
+import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.google.gson.GsonBuilder
+
 import org.json.JSONObject
 
 
@@ -28,9 +32,13 @@ class AccountDetailsFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+
+
     private lateinit var nomeUsuario: TextView
     private lateinit var emailUsuario: TextView
 
+    private lateinit var nomeUsuario2: EditText
+    private lateinit var emailUsuario2: EditText
     private lateinit var phoneUsuario: EditText
     private lateinit var address1Usuario: EditText
     private lateinit var address2Usuario: EditText
@@ -40,15 +48,12 @@ class AccountDetailsFragment : Fragment() {
 
 
 
-
-
     private lateinit var auth: FirebaseAuth
     private var gson = GsonBuilder().enableComplexMapKeySerialization().create()
 
 
-    val db: FirebaseFirestore = FirebaseFirestore.getInstance()
-    lateinit var usuarioID: String
-
+    private var db = Firebase.firestore
+    private lateinit var userId: String
 
 
     override fun onCreateView(
@@ -58,7 +63,51 @@ class AccountDetailsFragment : Fragment() {
 
         _binding = FragmentAccountDetailsBinding.inflate(inflater, container, false)
 
-        IniciarComponentes()
+        userId = FirebaseAuth.getInstance().currentUser!!.uid
+
+        nomeUsuario2=binding.tvNomeProfile2
+        emailUsuario2=binding.tvEmailProfile2
+        phoneUsuario=binding.tvPhoneProfile2
+        address1Usuario=binding.tvEndereco1Profile2
+        address2Usuario=binding.tvEndereco2Profile2
+        address3Usuario=binding.tvEndereco3Profile2
+        curriculoUsuario=binding.tvCurriculoProfile2
+
+
+        btn_update=binding.btnUpdate
+
+
+        btn_update.setOnClickListener{
+
+            val sNomeUsuario = nomeUsuario2.text.toString().trim()
+            val sEmailUsuario = emailUsuario2.text.toString().trim()
+            val sNumeroUsuario = phoneUsuario.text.toString().trim()
+            val sAddress1Usuario = address1Usuario.text.toString().trim()
+            val sAddress2Usuario = address2Usuario.text.toString().trim()
+            val sAddress3Usuario = address3Usuario.text.toString().trim()
+            val sCurriculoUsuario = curriculoUsuario.text.toString().trim()
+
+
+            val mapUpdate = mapOf(
+                "name" to sNomeUsuario,
+                "email" to sEmailUsuario,
+                "phone" to sNumeroUsuario,
+                "address1" to sAddress1Usuario,
+                "address2" to sAddress2Usuario,
+                "address3" to sAddress3Usuario,
+                "curriculum" to sCurriculoUsuario
+
+            )
+            db.collection("dentista").document(userId).update(mapUpdate)
+                .addOnSuccessListener {
+                    Snackbar.make(requireView(),"Atualização de dados sucedida!", Snackbar.LENGTH_LONG).show()
+                }
+                .addOnFailureListener{
+                    Snackbar.make(requireView(),it.toString(), Snackbar.LENGTH_LONG).show()
+                }
+
+        }
+
 
         return binding.root
 
@@ -66,6 +115,7 @@ class AccountDetailsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
 
         auth = Firebase.auth
         val user = auth.currentUser
@@ -97,17 +147,43 @@ class AccountDetailsFragment : Fragment() {
 
 
 
-
         binding.btnVoltar.setOnClickListener {
             findNavController().navigate(R.id.action_accountDetailsFragment_to_homeMenuFragment)
         }
     }
 
+    private fun setFireStoreData() {
+        val ref = db.collection("dentista").document(userId)
+        ref.get().addOnSuccessListener {
+            if (it != null) {
+                val nomeUsuario = it.data?.get("name")?.toString()
+                val emailUsuario = it.data?.get("email")?.toString()
+                val numeroUsuario = it.data?.get("phone")?.toString()
+                val endereco1Usuario = it.data?.get("address1")?.toString()
+                val endereco2Usuario = it.data?.get("address2")?.toString()
+                val endereco3Usuario = it.data?.get("address3")?.toString()
+                val curriculo2Usuario = it.data?.get("address3")?.toString()
 
-    private fun IniciarComponentes() {
-        nomeUsuario = binding.textView
-        emailUsuario = binding.textView2
+
+                nomeUsuario2.setText(nomeUsuario)
+                emailUsuario2.setText(emailUsuario)
+                phoneUsuario.setText(numeroUsuario)
+                address1Usuario.setText(endereco1Usuario)
+                address2Usuario.setText(endereco2Usuario)
+                address3Usuario.setText(endereco3Usuario)
+                curriculoUsuario.setText(curriculo2Usuario)
+
+
+            }
+        }
+            .addOnFailureListener{
+                Snackbar.make(requireView(),"Falha ao colocar.", Snackbar.LENGTH_LONG).show()
+            }
+
     }
+
+
+
 
 
     override fun onDestroyView() {
