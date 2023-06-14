7e6d10d9aad6a83d30dd6ba45afebeaa66ec0634
Possanii <gpossanii@outlook.com>
2023-06-14 15:02:10 -0300

    chamados aceitos

--- app/src/main/java/br/com/puccampinas/uteeth3pi/recycleview/ChamadosAdapter.kt
+++ app/src/main/java/br/com/puccampinas/uteeth3pi/recycleview/ChamadosAdapter.kt
@@ -1,30 +1,18 @@
 package br.com.puccampinas.uteeth3pi.recycleview
 
-import android.content.ContentValues
-import android.content.ContentValues.TAG
 import android.content.Intent
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import br.com.puccampinas.uteeth3pi.ChamadosAceitosFragment
-import br.com.puccampinas.uteeth3pi.MainActivity
 import br.com.puccampinas.uteeth3pi.MapActivity
 import br.com.puccampinas.uteeth3pi.R
-import br.com.puccampinas.uteeth3pi.data
-import com.bumptech.glide.Glide
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import com.google.firebase.messaging.FirebaseMessaging
-import com.google.gson.GsonBuilder
-import org.json.JSONObject
 
-class ChamadosAdapter(private val items: List<DocumentSnapshot>) : RecyclerView.Adapter<ChamadosViewHolder>() {
+class ChamadosAdapter(private val items: List<DocumentSnapshot>) :
+    RecyclerView.Adapter<ChamadosViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChamadosViewHolder {
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item2, parent, false)
@@ -41,6 +29,9 @@ class ChamadosAdapter(private val items: List<DocumentSnapshot>) : RecyclerView.
         val longitude = holder.longitude
 
 
+        holder.tv_name.text = name;
+        holder.tv_phone.text = phone
+
 
         holder.btn_localizacao.setOnClickListener {
 
@@ -52,31 +43,28 @@ class ChamadosAdapter(private val items: List<DocumentSnapshot>) : RecyclerView.
         }
     }
 
-        fun enviarLocalizacaoParaFirestore(latitude: Double, longitude: Double) {
-            val db = FirebaseFirestore.getInstance()
+    fun enviarLocalizacaoParaFirestore(latitude: Double, longitude: Double) {
+        val db = FirebaseFirestore.getInstance()
 
-            val localizacao = hashMapOf(
-                "latitude" to latitude,
-                "longitude" to longitude
-            )
+        val localizacao = hashMapOf(
+            "latitude" to latitude,
+            "longitude" to longitude
+        )
 
-            db.collection("localizacoes")
-                .add(localizacao)
-                .addOnSuccessListener { documentReference ->
-                    // Sucesso ao enviar a localização
-                }
-                .addOnFailureListener { e ->
-                    // Erro ao enviar a localização
-                }
-        }
-
-
+        db.collection("localizacoes")
+            .add(localizacao)
+            .addOnSuccessListener { documentReference ->
+                // Sucesso ao enviar a localização
+            }
+            .addOnFailureListener { e ->
+                // Erro ao enviar a localização
+            }
+    }
 
 
     override fun getItemCount(): Int {
         return items.size
     }
-
 
 
 }
